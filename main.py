48618c84250c4fdc65089a551311ee9a6a3f0b07
run-fourest-run <alexander.edward.fournier@gmail.com>
2021-12-07 16:40:12 -0700

    both string interpolation and tx.run being used

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import json
-from collections import defaultdict
 from connection import connection
+from neo4j import GraphDatabase
 
 
 
 
 
 
-
-def read_json_file(json_file):
+def read_json_file(json_file_path):
     '''
     Reads Json File and yields a generator
     :param json_file:
     :return:
     '''
-    with open(json_file,'r') as f:
+    with open(json_file_path,'r') as f:
         for line in f:
             json_object = json.loads(line)
             yield json_object
@@ -34,13 +33,28 @@ def generate_parameter_data(json_object,properties:list):
 
 
 
+'''
+todo: I feel like it's kind of confusing to call the functions inside this function... should I create a decorator/wrapper? 
+'''
+def generate_properties(acceptable_properties:list,json_file_path:str):
+    '''
+    acceptable properties
+    :param acceptable_properties: list of properties that we want to extract from the json
+    :return: list of parameters
+    '''
+    parameter_list = []
+    for json_object in read_json_file(json_file_path):
+        for _dict in generate_parameter_data(json_object, acceptable_properties):
+            parameter_list.append(_dict)
+    return parameter_list
+
 
 def chunk_parameters(parameters:list,chunk_size:int):
     '''
     Chunks a list into smaller sublists. The idea here is to take create batches or chunks of parameters.
     :param parameters: input parameters
     :param chunk_size: size of sublists
-    :return:
+    :return: list of lists. sublists contain a fixed number of elements (the last sublist will just contain the remainder)
     '''
     chunks = [parameters[x:x+chunk_size] for x in range(0, len(parameters),chunk_size)]
     return chunks
@@ -48,42 +62,51 @@ def chunk_parameters(parameters:list,chunk_size:int):
 
 
 
-
-def generate_cypher(parameters):
+def generate_cypher(batches_of_paramters):
     '''
+    DON'T USE THIS FUNCTION.
+    I'm only keeping it for future reference but string interpolation isn't the move here.
 
     :param parameters: list of dicts representing parameters
-    :return: String that represents a cypher statement
+    :return: List of Dictionaries. Dictionary Key is the batch index. Value is the Cypher string with Parameters
 
 
-    {'params': [{'author': 'Dean R Koontz'}, {'author': 'Stephenie Meyer'}
+    :params {'params': [{'author': 'Dean R Koontz'}, {'author': 'Stephenie Meyer'}
     UNWIND $params as param
+    WITH param
     CREATE (u:User {author:param.author})
     '''
 
 
-    #yuck...
-    _parameters_key= 'params'
-    _parameters = defaultdict(list)
-    _parameters[_parameters_key] = parameters
-    #double yuck...
-    _parameters = str(dict(_parameters))
-
-    unwind_string = ':params '  + _parameters + '\n ' + 'UNWIND $params as param'
-
-
-
-
-
-    base_query = "CREATE (u:User {{ {properties} }})"
-    properties = ', '.join(('{0}: param.{0}'.format(v) for v in parameters[0]))
-    final_templated_base_query = base_query.format(properties=properties)
-
-    final_cypher_query = unwind_string + '\n'  + final_templated_base_query
-    return final_cypher_query
+    cypher_statements = []
+    i = 0
+    for batch in batches_of_parameters:
+        while i < len(batches_of_paramters):
+            i = i+1
+            cypher = f'UNWIND ${str(batch)} as param CREATE (u:User {{author:param.author}}'
+            cypher_statement = {i:cypher}
+            cypher_statements.append(cypher_statement)
+    return cypher_statements
 
 
 
+def run_string_interpolation(cypher_statements:list,connection):
+    '''
+    DON'T USE THIS FUNCTION.
+    I'm only keeping it for future reference but string interpolation isn't the move here.
+    This uses the Connection module as well.
+
+    :param cypher_statements: list of Cypher statements
+    :param connection: Connection object
+    :return:
+    '''
+    for cypher_dict in cypher_statements:
+        for k,cypher_statement in cypher_dict.items():
+            connection.execute(cypher_statement)
+
+
+def create_authors(tx,batch):
+    tx.run(f"UNWIND $batch as param CREATE (u:User {{author:param:author}})")
 
 
 
@@ -94,22 +117,20 @@ def generate_cypher(parameters):
 
 
 if __name__ == '__main__':
-    json_file =  '/Users/alexanderfournier/PycharmProjects/json_neo4j/input/nyt2.json'
-    author_properties = ['author']
-
-    author_parameter_list = []
-    for json_object in read_json_file(json_file):
-        for _dict in generate_parameter_data(json_object,author_properties):
-            author_parameter_list.append(_dict)
+    json_file_path =  '/Users/alexanderfournier/PycharmProjects/json_neo4j/input/nyt2.json'
+    acceptable_properties = ['author']
+    uri = 'bolt://localhost:7687'
+    driver = GraphDatabase.driver(uri, auth=('neo4j','Reddit123!'))
 
 
 
-    chunks = chunk_parameters(author_parameter_list,chunk_size=1000)
+    parameter_list = generate_properties(acceptable_properties,json_file_path)
+    batches_of_parameters = chunk_parameters(parameter_list,chunk_size=50)
 
 
-
-    cypher_statements = [generate_cypher(chunk) for chunk in chunks]
-
+    with driver.session() as session:
+        for batch in batches_of_parameters:
+            session.write_transaction(create_authors,batch)
 
 
 
@@ -119,10 +140,15 @@ if __name__ == '__main__':
 
 
 
-    connection  = connection.Neo4j(url='bolt://localhost:7687',database='test',password='Reddit123!',username='neo4j')
 
-    for cypher_statement in cypher_statements:
-        connection.write(cypher_statement)
+
+
+
+
+
+
+
+
 
 
 
